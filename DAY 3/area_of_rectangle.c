d9f43cedfc63813f2edc9da06e1c4b30dbb7a1fd
Nishan <nissandhakal11@gmail.com>
2024-06-23 18:31:29 +0545

    Update

--- DAY 3/area_of_rectangle.c
+++ DAY 3/area_of_rectangle.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int length, width, area;
+    float length, width, area;
     printf("Enter length and width of the rectangle: ");
-    scanf("%d %d", &length, &width);
+    scanf("%f %f", &length, &width);
     area = length * width;
-    printf("Area of the rectangle: %d\n", area);
+    printf("Area of the rectangle: %.2f\n", area);
     return 0;
 }
